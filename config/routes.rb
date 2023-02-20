917b052ae613a6e64e974bb539654b5c8e63a978
kg8m <takumi.kagiyama@gmail.com>
2023-02-21 00:38:44 +0900

    v0.0.3 - inherit the application's `default_url_options` but should be isolated

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Tanshuku::Engine.routes.draw do
-  default_url_options Rails.application.routes.default_url_options
+  default_url_options Rails.application.routes.default_url_options.deep_dup
 
   get "/:key", to: "urls#show"
 end
