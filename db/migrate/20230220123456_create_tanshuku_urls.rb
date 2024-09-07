28bae031ff9a2e5717c89e0e68f69fa6e490596c
kg8m <takumi.kagiyama@gmail.com>
2024-09-07 21:55:33 +0900

    Add comments to synchronize the `limit` of database columns’ and `Tanshuku` configs

--- db/migrate/20230220123456_create_tanshuku_urls.rb
+++ db/migrate/20230220123456_create_tanshuku_urls.rb
@@ -4,8 +4,13 @@ class CreateTanshukuUrls < ActiveRecord::Migration[7.0]
   def change
     create_table :tanshuku_urls do |t|
       t.text :url, null: false
+
+      # You might adjust the `limit: 128` depending on `Tanshuku.config.url_hasher`.
       t.string :hashed_url, null: false, limit: 128, index: { unique: true }, comment: "cf. Tanshuku::Url.hash_url"
+
+      # You might adjust the `limit: 20` depending on `.key_length` and `.key_generator` of `Tanshuku.config`.
       t.string :key, null: false, limit: 20, index: { unique: true }, comment: "cf. Tanshuku::Url.generate_key"
+
       t.datetime :created_at, null: false, precision: nil
     end
   end
